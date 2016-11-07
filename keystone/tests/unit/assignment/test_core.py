e88c52e1c504bd0afa3c8fd93ce18e01b80d0c04
Adam Young <ayoung@redhat.com>
2016-11-06 21:41:50 -0500

    move up role backend tests
    Change-Id: I586607956346c90a655521014551e27a3f497793

--- keystone/tests/unit/assignment/test_core.py
+++ keystone/tests/unit/assignment/test_core.py
@@ -15,6 +15,7 @@
 import copy
 import uuid
 
+from keystone.common import driver_hints
 from keystone import exception
 from keystone.tests import unit
 from keystone.tests.unit import default_fixtures
@@ -121,3 +122,155 @@ class RoleTests(object):
         self.assertRaises(exception.RoleNotFound,
                           self.role_api.get_role,
                           role_id)
+
+    def test_implied_role_crd(self):        
+        role1 = unit.new_role_ref(name= uuid.uuid4().hex)
+        self.role_api.create_role(role1['id'], role1)
+        role2 = unit.new_role_ref(name= uuid.uuid4().hex)
+        self.role_api.create_role(role2['id'], role2)
+        # Create
+        implied_role_created = self.role_api.create_implied_role(
+            role1['id'], role2['id'])
+        self.assertEquals(role1['id'], implied_role_created['prior_role_id'])
+        self.assertEquals(role2['id'], implied_role_created['implied_role_id'])
+
+        # Read 
+        implied_role1 = self.role_api.get_implied_role(role1['id'], role2['id'])
+        self.assertEquals(implied_role_created, implied_role1)
+        self.assertEquals(role1['id'], implied_role1['prior_role_id'])
+        self.assertEquals(role2['id'], implied_role1['implied_role_id'])
+
+
+        role3 = unit.new_role_ref(name= uuid.uuid4().hex)
+        self.role_api.create_role(role3['id'], role3)
+
+        self.assertRaises(exception.ImpliedRoleNotFound,
+                          self.role_api.get_implied_role,
+                          role1['id'], role3['id'])
+        
+        self.role_api.create_implied_role(role1['id'], role3['id'])
+        
+        implied_role2 = self.role_api.get_implied_role(role1['id'], role3['id'])
+        self.assertEquals(role1['id'], implied_role2['prior_role_id'])
+        self.assertEquals(role3['id'], implied_role2['implied_role_id'])
+
+        # Delete
+        implied_list = self.role_api.list_implied_roles(role1['id'])
+        self.assertEquals(2, len(implied_list))
+        self.assertIn(implied_role1, implied_list)
+        self.assertIn(implied_role2, implied_list)
+
+        self.role_api.delete_implied_role(role1['id'], role3['id'])
+        implied_list = self.role_api.list_implied_roles(role1['id'])
+        self.assertEquals(1, len(implied_list))
+        self.assertIn(implied_role1, implied_list)
+
+    @wip('This test does not pass yet.')
+    def test_deleting_role_removes_inference_rule(self):
+    
+        role1 = unit.new_role_ref(name= uuid.uuid4().hex)
+        self.role_api.create_role(role1['id'], role1)
+        role2 = unit.new_role_ref(name= uuid.uuid4().hex)
+        self.role_api.create_role(role2['id'], role2)
+        self.role_api.create_implied_role(role1['id'], role2['id'])
+
+        implied_role1 = self.role_api.get_implied_role(role1['id'], role2['id'])
+
+        role3 = unit.new_role_ref(name= uuid.uuid4().hex)
+        self.role_api.create_role(role3['id'], role3)        
+        self.role_api.create_implied_role(role1['id'], role3['id'])
+        
+        implied_role2 = self.role_api.get_implied_role(role1['id'], role3['id'])
+
+        implied_list = self.role_api.list_implied_roles(role1['id'])
+        self.assertEquals(2, len(implied_list))
+        self.assertIn(implied_role1, implied_list)
+        self.assertIn(implied_role2, implied_list)
+
+        self.role_api.delete_role(role3['id'])
+        implied_list = self.role_api.list_implied_roles(role1['id'])
+        self.assertEquals(1, len(implied_list))
+        self.assertIn(implied_role1, implied_list)
+        self.assertIn(implied_role2, implied_list)
+                
+    def test_url_pattern_crud(self):
+        # Create
+        service1 = uuid.uuid4().hex
+        url_pattern_ref = unit.new_url_pattern_ref()
+        url_pattern_ref['service'] = service1
+        url_pattern_1_created = self.role_api.create_url_pattern(
+            url_pattern_ref['id'], url_pattern_ref)
+        self.assertEquals(url_pattern_1_created, url_pattern_ref)
+
+        # Read: Get
+        url_pattern_1 = self.role_api.get_url_pattern(url_pattern_ref['id'])
+        self.assertEquals(url_pattern_1, url_pattern_ref)
+        
+
+        url_pattern_ref = unit.new_url_pattern_ref()
+        url_pattern_ref['service'] = service1
+        url_pattern_2_created = self.role_api.create_url_pattern(
+            url_pattern_ref['id'], url_pattern_ref)
+
+
+        service2 = uuid.uuid4().hex
+        url_pattern_ref = unit.new_url_pattern_ref()
+        url_pattern_ref['service'] = service2
+        url_pattern_3_created = self.role_api.create_url_pattern(
+            url_pattern_ref['id'], url_pattern_ref)
+
+        # Read:  List all
+        all_url_patterns = self.role_api.list_url_patterns()
+        self.assertEquals(3, len(all_url_patterns))
+
+        hints = driver_hints.Hints()
+        all_url_patterns = self.role_api.list_url_patterns(hints)
+        self.assertEquals(3, len(all_url_patterns))
+
+
+        hints.add_filter('service', service1)
+        service_1_url_patterns = self.role_api.list_url_patterns(hints)
+        self.assertEquals(2, len(service_1_url_patterns))
+        self.assertIn(url_pattern_1_created, service_1_url_patterns)
+        self.assertIn(url_pattern_2_created, service_1_url_patterns)
+        self.assertNotIn(url_pattern_3_created, service_1_url_patterns)
+
+        hints = driver_hints.Hints()
+        hints.add_filter('service', service2)
+        service_2_url_patterns = self.role_api.list_url_patterns(hints)
+        self.assertEquals(1, len(service_2_url_patterns))
+        self.assertNotIn(url_pattern_1_created, service_2_url_patterns)
+        self.assertNotIn(url_pattern_2_created, service_2_url_patterns)
+        self.assertIn(url_pattern_3_created, service_2_url_patterns)
+
+
+        # Update
+        url_pattern_1_id = url_pattern_1['id']
+        
+        url_pattern_ref = unit.new_url_pattern_ref()
+        url_pattern_ref['id'] = url_pattern_1_created['id']
+        url_pattern_1_updated = self.role_api.update_url_pattern(
+            url_pattern_1_id,
+            url_pattern_ref)
+
+        url_pattern_1_gotten = self.role_api.get_url_pattern(
+            url_pattern_1_id)
+        
+        self.assertEquals(url_pattern_ref, url_pattern_1_updated)
+        self.assertEquals(url_pattern_1_gotten, url_pattern_1_updated)
+        
+        #Delete
+        all_url_patterns = self.role_api.list_url_patterns()
+        self.assertEquals(3, len(all_url_patterns))
+        self.assertIn(url_pattern_1_gotten, all_url_patterns)
+        self.assertIn(url_pattern_2_created, all_url_patterns)
+        self.assertIn(url_pattern_3_created, all_url_patterns)
+
+        
+        self.role_api.delete_url_pattern(url_pattern_1_id)
+        all_url_patterns = self.role_api.list_url_patterns()
+        self.assertEquals(2, len(all_url_patterns))
+
+        self.assertNotIn(url_pattern_1_gotten, all_url_patterns)
+        self.assertIn(url_pattern_2_created, all_url_patterns)
+        self.assertIn(url_pattern_3_created, all_url_patterns)
